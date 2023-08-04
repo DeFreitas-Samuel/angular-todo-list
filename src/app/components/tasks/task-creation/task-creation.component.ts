2cd8a35f75a469b32ab127efc50b37d5041f0502
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2023-08-04 01:03:29 -0400

    Trying to add new task by specifying a list too

--- src/app/components/tasks/task-creation/task-creation.component.ts
+++ src/app/components/tasks/task-creation/task-creation.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
 import {priority} from 'src/models/enums/priority.enum';
 import {TaskService} from 'src/services/task.service';
@@ -11,7 +11,18 @@ import {Router} from '@angular/router';
   templateUrl: './task-creation.component.html',
   styleUrls: ['./task-creation.component.scss']
 })
-export class TaskCreationComponent {
+interface taskFormValue {
+  title: string | null |undefined,
+  dueDate: string | null |undefined,
+  priority: priority | null |undefined,
+  taskList: string | null |undefined
+}
+
+export class TaskCreationComponent implements OnInit{
+
+
+
+  public taskLists: {id: string, name:string}[] = [];
 
   public priorities: string[] = Object.values(priority);
 
@@ -19,9 +30,18 @@ export class TaskCreationComponent {
     title: ['', Validators.required],
     dueDate: [priority.Medium, Validators.required],
     priority: ['',  Validators.required],
+    taskList: ['', Validators.required]
   });
 
   constructor(private fb: FormBuilder, private taskService: TaskService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.bootstrap()
+  }
+
+  private bootstrap() {
+    this.getTaskLists();
+  }
 
   public get priority(): typeof priority {
     return priority;
@@ -29,28 +49,40 @@ export class TaskCreationComponent {
 
   public onCreateTask() {
     console.log(this.taskForm.value);
-    const newTask = this.convertFormValueToTask(this.taskForm.value);
+    const newTask = this.convertFormValueToTask(this.taskForm.value!);
     console.log("This is the new task", newTask);
-    this.taskService.addNewTask( this.taskService.tasksSnapshot[0].id, newTask);
+    console.log("Before insertion", this.taskService.tasksSnapshot);
+    this.taskService.addNewTask( this.taskForm.value.taskList!, newTask);
+    console.log("After insertion", this.taskService.tasksSnapshot);
     this.taskForm.reset();
     this.router.navigate(['/']);
 
   }
 
-  private convertFormValueToTask(formValue: any): Task {
+  private convertFormValueToTask(formValue: Partial<taskFormValue>): Task {
 
-    const formTitle = formValue?.title;
-    const formDueDate = new Date(formValue?.dueDate);
-    const formPriority: priority = formValue?.priority;
-
-
-
-
-    return new Task(formTitle, formDueDate, formPriority, false, []);
-
-
+    if(formValue.title && formValue.dueDate && formValue.priority){
+      const formTitle = formValue?.title;
+      const formDueDate:Date = new Date(formValue?.dueDate);
+      const formPriority: priority = formValue?.priority;
+      return new Task(formTitle, formDueDate, formPriority, false, []);
+    }
+    else {
+      throw new Error("Invalid form value. Title, due date, and priority are required.");
+    }
 
   }
+
+  private getTaskLists(){
+    this.taskService.tasksSnapshot.map((taskList)=>{
+      const taskListSimplified = {
+        id: taskList.id,
+        name: taskList.name
+      }
+      this.taskLists.push(taskListSimplified);
+    })
+  }
+
 
 
 }
