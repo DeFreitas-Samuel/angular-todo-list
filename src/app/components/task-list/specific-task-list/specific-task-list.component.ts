5ee7cab9df6690164386e84b3ca8e7d796b952e8
DeFreitas-Samuel <defreitas.samuel.99@gmail.com>
2023-09-13 16:03:35 -0400

    some styles changes

--- src/app/components/task-list/specific-task-list/specific-task-list.component.ts
+++ src/app/components/task-list/specific-task-list/specific-task-list.component.ts
@@ -1,24 +1,23 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {TaskList} from 'src/models/taskList';
-import {TaskListUtilities} from 'src/app/shared/utilities/task-list.utilities'
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { TaskList } from 'src/models/taskList';
+import { TaskListUtilities } from 'src/app/shared/utilities/task-list.utilities';
 
 @Component({
   selector: 'app-specific-task-list',
   templateUrl: './specific-task-list.component.html',
-  styleUrls: ['./specific-task-list.component.scss']
+  styleUrls: ['./specific-task-list.component.scss'],
 })
-export class SpecificTaskListComponent implements OnInit {
-
+export class SpecificTaskListComponent implements OnChanges {
   @Input() taskList!: TaskList;
   taskTotalLength: number = 0;
   unfinishedTasksLength: number = 0;
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
     this.taskTotalLength = TaskListUtilities.getTotalTasks(this.taskList);
-    this.unfinishedTasksLength = TaskListUtilities.getTotalUnfinishedTasks(this.taskList);
+    this.unfinishedTasksLength = TaskListUtilities.getTotalUnfinishedTasks(
+      this.taskList
+    );
   }
-
 }
